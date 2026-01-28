4af5f63e0755ad6ffe3095db06300dbffdc7a95c
Kevin Massol <kevin.massol@icloud.com>
2026-01-28 16:16:05 +0100

    Initial Commit

---
+++ swiftfulthinking-todolist/swiftfulthinking_todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftfulthinking_todolistApp.swift
+//  swiftfulthinking-todolist
+//
+//  Created by KEVIN on 28/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct swiftfulthinking_todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
